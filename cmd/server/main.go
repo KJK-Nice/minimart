c9d28f386baab2322545c5962d99f2f51f1c2740
khajornkiat <khajornkiat.us@ols.co.th>
2025-08-11 22:02:35 +0700

    [ci] Add Health check path

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -136,6 +136,12 @@ func main() {
 		})
 	})
 
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status": "ok",
+		})
+	})
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
